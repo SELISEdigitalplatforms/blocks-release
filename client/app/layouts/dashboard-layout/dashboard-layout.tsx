83f4eac6cb21df535af0ddd713257694bb04b4dc
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-05-12 20:54:43 +0600

    add: error boundary

--- client/app/layouts/dashboard-layout/dashboard-layout.tsx
+++ client/app/layouts/dashboard-layout/dashboard-layout.tsx
@@ -3,6 +3,7 @@ import { DashboardLayoutProvider } from "@/contexts/dashboard-layout-provider";
 import { ProtectedGuard } from "@/guards/protected-guard";
 import { SidebarMenuDesktop } from "@/layouts/sidebar-menu-desktop/sidebar-menu-desktop";
 import { DashboardHeader } from "@/layouts/dashboard-header/dashboard-header";
+import { ErrorBoundary } from "@/components/error-boundary";
 
 export function DashboardLayout() {
   return (
@@ -13,7 +14,9 @@ export function DashboardLayout() {
           <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
             <DashboardHeader />
             <main className="relative flex-1 overflow-auto p-2 sm:p-4 lg:p-6">
-              <Outlet />
+              <ErrorBoundary>
+                <Outlet />
+              </ErrorBoundary>
             </main>
           </div>
         </div>

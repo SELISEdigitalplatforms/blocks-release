722aa3d8a9cbbcd60f2650f1a4ec102ebc768109
Sk. Zaber Ahmed <zaber.ahmed@selisegroup.com>
2026-07-23 18:40:51 +0600

    add: e2e tests

---
+++ e2e/constants/test-data.constant.ts
@@ -0,0 +1,8 @@
+export default {
+  TEST_REPO_OWNER: "zaber-ahmed-selise",
+  TEST_REPO_NAME: "deployment_test",
+  ENVIRONMENTS: {
+    DEVELOPMENT: "Development",
+    TESTING: "Testing",
+  },
+};
